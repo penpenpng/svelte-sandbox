92fe6560cc757cf2c22b581cb3f7c44da3968a6b
SnowCait <SnowCait@users.noreply.github.com>
2023-06-12 06:30:23 +0900

    Dialog page

---
+++ tribute/src/routes/dialog/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let textarea: HTMLTextAreaElement;
+
+	onMount(async () => {
+		const { default: Tribute } = await import('tributejs');
+
+		const tribute = new Tribute({
+			values: [
+				{ key: 'Phil Heartman', value: 'pheartman' },
+				{ key: 'Gordon Ramsey', value: 'gramsey' }
+			]
+		});
+		tribute.attach(textarea);
+	});
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<textarea bind:this={textarea} />
