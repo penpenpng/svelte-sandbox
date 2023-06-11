6dcae2e3f5f38a80fc632531c2d2bf3c6a125863
SnowCait <SnowCait@users.noreply.github.com>
2023-06-12 06:30:51 +0900

    Modal dialog

--- tribute/src/routes/dialog/+page.svelte
+++ tribute/src/routes/dialog/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
+	let dialog: HTMLDialogElement;
 	let textarea: HTMLTextAreaElement;
 
 	onMount(async () => {
@@ -13,9 +14,12 @@
 			]
 		});
 		tribute.attach(textarea);
+		dialog.showModal();
 	});
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<textarea bind:this={textarea} />
+<dialog bind:this={dialog}>
+	<textarea bind:this={textarea} />
+</dialog>
