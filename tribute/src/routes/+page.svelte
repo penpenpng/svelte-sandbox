923d5169cea9743e2cb763a2ed09aef5eff9d305
SnowCait <SnowCait@users.noreply.github.com>
2023-06-11 16:59:50 +0900

    zurb/tribute

--- tribute/src/routes/+page.svelte
+++ tribute/src/routes/+page.svelte
@@ -1,2 +1,22 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const { default: Tribute } = await import('tributejs');
+
+		var tribute = new Tribute({
+			values: [
+				{ key: 'Phil Heartman', value: 'pheartman' },
+				{ key: 'Gordon Ramsey', value: 'gramsey' }
+			]
+		});
+		const textarea = document.getElementById('example');
+		if (textarea !== null) {
+			tribute.attach(textarea);
+		}
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<textarea id="example" />
