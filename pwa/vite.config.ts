fa398a568f7c9831ba3bf9a5f2f4da99fa23268c
SnowCait <SnowCait@users.noreply.github.com>
2023-09-30 17:13:26 +0900

    PWA

--- pwa/vite.config.ts
+++ pwa/vite.config.ts
@@ -1,6 +1,33 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			strategies: 'generateSW',
+			manifest: {
+				name: 'Svelte-PWA',
+				short_name: 'Svelte-PWA',
+				description: 'Svelte PWA example',
+				theme_color: '#ffffff',
+				icons: [
+					{
+						src: '192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: '512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			},
+			devOptions: {
+				enabled: true
+			}
+		})
+	]
 });
