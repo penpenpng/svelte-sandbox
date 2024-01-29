74f074ab0db787c0b711b062d6b88fa3dfe50d9a
SnowCait <SnowCait@users.noreply.github.com>
2024-01-29 12:10:45 +0900

    Counter

---
+++ rx-nostr/src/routes/api/count/+server.ts
@@ -0,0 +1,8 @@
+import type { RequestHandler } from "@sveltejs/kit";
+
+let i = 0;
+
+export const GET: RequestHandler = async ({ url }) => {
+    i++;
+    return new Response(JSON.stringify(i));
+};
