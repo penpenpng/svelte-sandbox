c815fe2afe0e96ce87cf8051fa853a266aac8ba5
SnowCait <SnowCait@users.noreply.github.com>
2024-02-21 09:39:48 +0900

    Menu

--- svelteui/src/routes/+page.svelte
+++ svelteui/src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Menu } from '@svelteuidev/core';
+	import IconRepeat from '@tabler/icons-svelte/dist/svelte/icons/IconRepeat.svelte';
+	import IconQuote from '@tabler/icons-svelte/dist/svelte/icons/IconQuote.svelte';
+
+	let items = [0];
+
+	setInterval(() => {
+		items.unshift(items.length);
+		items = items;
+	}, 1000);
+</script>
+
+<h1>SvelteUI</h1>
+<ul>
+	{#each items as item (item)}
+		<li>
+			<div>{item}</div>
+			<Menu>
+				<svelte:fragment slot="control">
+					<IconRepeat></IconRepeat>
+				</svelte:fragment>
+				{#if item % 2 === 0}
+					<Menu.Label>App</Menu.Label>
+				{/if}
+				<Menu.Item icon={IconRepeat} on:click={() => console.log('repost', item)}>Repost</Menu.Item>
+				<Menu.Item icon={IconQuote}>Quote</Menu.Item>
+			</Menu>
+		</li>
+	{/each}
+</ul>
+
+<style>
+	ul {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	li + li {
+		border-top: 1px solid lightgray;
+	}
+</style>
