de7494d393fcaca215c77abad638c7c9b669e4a9
SnowCait <SnowCait@users.noreply.github.com>
2024-05-04 11:15:14 +0900

    Count

--- rx-nostr/src/routes/performance/+page.svelte
+++ rx-nostr/src/routes/performance/+page.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
 	import { rxNostr } from '$lib/Constants';
+	import type { Event } from 'nostr-typedef';
 	import { createRxBackwardReq, uniq } from 'rx-nostr';
 	import { onDestroy, onMount } from 'svelte';
+
+	const events = new Map<string, Event>();
 
 	const req = createRxBackwardReq();
 	const subscription = rxNostr
 		.use(req)
 		.pipe(uniq())
-		.subscribe(({ event, from }) => {
-			console.log(from, event);
+		.subscribe({
+			next: ({ event, from }) => {
+				console.log(from, event);
+				events.set(event.id, event);
+			},
+			complete: () => console.log('[count]', events.size)
 		});
 
 	onMount(() => {
