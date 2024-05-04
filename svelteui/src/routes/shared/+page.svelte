e5e8993d6640f38c6545edc0ee8cb90d085b9483
SnowCait <SnowCait@users.noreply.github.com>
2024-05-05 07:23:58 +0900

    Web Share Target APi

---
+++ svelteui/src/routes/shared/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+
+	let url = '';
+
+	onMount(() => {
+		url = $page.url.searchParams.get('url') ?? '';
+	});
+</script>
+
+<h1>Shared</h1>
+
+<p>{url}</p>
+
+<a href="/">Top</a>
