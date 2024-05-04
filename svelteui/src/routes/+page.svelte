388d179f8c739b0ea5b55e0da5c5fa63d84b47d9
SnowCait <SnowCait@users.noreply.github.com>
2024-05-05 05:02:30 +0900

    Web Share API

--- svelteui/src/routes/+page.svelte
+++ svelteui/src/routes/+page.svelte
@@ -5,5 +5,6 @@
 
 <ul>
 	<li><a href="/autocomplete">Autocomplete</a></li>
+	<li><a href="/share">Share</a></li>
 	<li><a href="/timeline">Timeline</a></li>
 </ul>
---
+++ svelteui/src/routes/share/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	async function share(): Promise<void> {
+		const data: ShareData = { text: 'test', url: 'https://example.com/', title: 'title' };
+		console.log('[share]', navigator.canShare === undefined, navigator.canShare(data));
+		await navigator.share(data);
+	}
+</script>
+
+<h1>Share</h1>
+
+<button on:click={share}>Share</button>
