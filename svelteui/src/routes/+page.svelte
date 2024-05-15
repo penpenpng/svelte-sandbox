9a62f73736654a6576671ffc61c220d1bddfb136
SnowCait <SnowCait@users.noreply.github.com>
2024-05-16 05:33:47 +0900

    File picker

--- svelteui/src/routes/+page.svelte
+++ svelteui/src/routes/+page.svelte
@@ -7,4 +7,5 @@
 	<li><a href="/autocomplete">Autocomplete</a></li>
 	<li><a href="/share">Share</a></li>
 	<li><a href="/timeline">Timeline</a></li>
+	<li><a href="/file-picker">File picker</a></li>
 </ul>
---
+++ svelteui/src/routes/file-picker/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	let files: FileList | undefined;
+</script>
+
+<h1>File picker</h1>
+
+<input type="file" multiple={true} bind:files accept="image/*" />
+
+{#if files !== undefined}
+	<ul>
+		{#each [...files] as file}
+			<li>
+				<span>{file.name}</span>
+				<span>({new Date(file.lastModified).toLocaleString()})</span>
+			</li>
+		{/each}
+	</ul>
+{/if}
