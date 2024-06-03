88029d63fe03c33a72f1c2daa1d4e0820f31854a
SnowCait <SnowCait@users.noreply.github.com>
2024-06-03 14:06:00 +0900

    OGP

--- pwa/src/routes/ssr/+page.svelte
+++ pwa/src/routes/ssr/+page.svelte
@@ -4,6 +4,11 @@
 	export let data: PageServerData;
 </script>
 
+<svelte:head>
+	<meta property="og:title" content={data.event?.content} />
+	<meta property="og:type" content="website" />
+</svelte:head>
+
 <h1>SSR</h1>
 
 <p>{data.event?.content}</p>
