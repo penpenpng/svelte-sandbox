acea44c16322f5275ef6eec985827e6ed72c15de
SnowCait <SnowCait@users.noreply.github.com>
2024-06-03 14:32:24 +0900

    Reactive

--- pwa/src/routes/ssr/+page.svelte
+++ pwa/src/routes/ssr/+page.svelte
@@ -2,13 +2,15 @@
 	import type { PageServerData } from './$types';
 
 	export let data: PageServerData;
+
+	$: content = data.event?.content;
 </script>
 
 <svelte:head>
-	<meta property="og:title" content={data.event?.content} />
+	<meta property="og:title" {content} />
 	<meta property="og:type" content="website" />
 </svelte:head>
 
 <h1>SSR</h1>
 
-<p>{data.event?.content}</p>
+<p>{content}</p>
