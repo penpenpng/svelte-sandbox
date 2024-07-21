a0a88374e51370d8c7cb1cd333339994c852202b
SnowCait <SnowCait@users.noreply.github.com>
2024-07-21 12:24:12 +0900

    Debug

--- rx-nostr/src/routes/performance/+page.svelte
+++ rx-nostr/src/routes/performance/+page.svelte
@@ -1,27 +1,29 @@
 <script lang="ts">
 	import type { Event } from 'nostr-typedef';
 	import { createRxBackwardReq, createRxNostr, uniq } from 'rx-nostr';
-	import { createVerificationServiceClient } from 'rx-nostr-crypto';
+	import { createVerificationServiceClient, verifier } from 'rx-nostr-crypto';
 	import { onDestroy, onMount } from 'svelte';
 
 	const events = new Map<string, Event>();
 
 	const verificationClient = createVerificationServiceClient();
 	const rxNostr = createRxNostr({
+		connectionStrategy: 'lazy-keep',
 		verifier: verificationClient.verify
+		// verifier: verifier
 	});
 
 	rxNostr.setDefaultRelays([
-		'wss://yabu.me/',
+		// 'wss://yabu.me/',
 		'wss://nos.lol/',
 		'wss://r.kojira.io/',
 		// 'wss://nostream.ocha.one/',
 		'wss://relay-jp.nostr.wirednet.jp/',
 		'wss://nostr.holybea.com/',
-		'wss://nostrja-kari.heguro.com/',
+		// 'wss://nostrja-kari.heguro.com/',
 		'wss://relay.nostr.band/',
 		'wss://nostr.fediverse.jp/',
-		'wss://relay-jp.nostr.moctane.com/',
+		// 'wss://relay-jp.nostr.moctane.com/',
 		'wss://nrelay-jp.c-stellar.net/',
 		'wss://riray.nostr1.com/',
 		'wss://ren.nostr1.com/',
@@ -33,8 +35,9 @@
 		.use(req)
 		.pipe(uniq())
 		.subscribe({
-			next: ({ event }) => {
-				console.log('ok', event.id);
+			next: ({ event, from }) => {
+				console.log('ok', from, event.id);
+				events.set(event.id, event);
 			},
 			complete: () => console.log('[count]', events.size)
 		});
